872e633bf20b1c00ef82ec680cb173c818f0958d
root <root@ubuntu>
2017-09-13 16:26:36 +0800

    commit at 4:26pm 9.12

--- trafficData/static_process/get_X_data.py
+++ trafficData/static_process/get_X_data.py
@@ -7,16 +7,29 @@ Created on 2017年9月9日
 import pandas as pd 
 import os
 import matplotlib.pyplot as plt
+import sklearn.preprocessing as preprocessing
+from pandas import Series
+import numpy as np
+import seaborn as sns 
+last_day=Series()
 df_final=pd.DataFrame()
 for filename in os.listdir("add_Week_result"):
     df=pd.read_csv("add_Week_result/"+filename)
-
-    df_row=df[df["监测点id"]==20104605]
-    week=df[df["监测点id"]==20104605]["Week"].as_matrix()[0]
+    
+    df_row=df[df["监测点id"]==10100406]
+    week=df[df["监测点id"]==10100406]["Week"].as_matrix()[0]
     
     df_temp=df_row.filter( regex="[0-9].*")
-    df_new=pd.DataFrame({"nums":df_temp.T[df_temp.T.columns[0]],"time_id":range(0,144)})
+    df_new=pd.DataFrame({"nums":df_temp.T[df_temp.T.columns[0]].as_matrix(),"time_id":range(0,144)})
     df_new['week']=week
+    if  not last_day.empty:
+        df_new['last_day']=last_day
+        last_day=df_new['nums']
+    else:
+        df_new['last_day']=df_new['nums']
+        last_day=df_new['nums']
+        #print 
+
     #print df_new
     if not df_final.empty:
         df_final=pd.concat([df_final,df_new])
@@ -36,13 +49,15 @@ for index ,row in df_final.iterrows():
         df_final.loc[index,'is_high']=1
     else:
         df_final.loc[index,'is_high']=0
+        
+
 dummies_Time=pd.get_dummies(df_final["time_id"], prefix="time_id")
 dummies_week=pd.get_dummies(df_final["week"], prefix="week")
 df_final= pd.concat([df_final,dummies_Time,dummies_week], axis=1)
 df_final.drop(['time_id','week'], axis=1, inplace=True)
 
 a=df_final.as_matrix()
-df_final.to_csv("X_default.csv",index=False)
+df_final.to_csv("X_default_history.csv",index=False)
 
 fig = plt.figure()
 fig.set(alpha=0.2)  # 设定图表颜色alpha参数
--- trafficData/static_process/learn_linear.py
+++ trafficData/static_process/learn_linear.py
@@ -5,28 +5,42 @@ from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import cross_validation
+import sklearn.preprocessing as preprocessing
+import seaborn as sns
 
-train_df=pd.read_csv("X_default.csv")
+def do_normalise(im):
+    return -np.log(1/((1 + im)/3601) - 1)
+
+def undo_normalise(im):
+    return (1/(np.exp(-im) + 1) * 3601 - 1).astype("uint8")
+
+train_df=pd.read_csv("X_default_history.csv")
 train_np=train_df.as_matrix()
-default=train_df.filter(regex="default_*")[:144]
-print default[:144]
+default=train_df.filter(regex="default*")[:144]
+#print default[:144]
 X=train_np[:,1:]
 
 y=train_np[:,0]
+print y.max()
 #print y
+
+
+
 clf=linear_model.LinearRegression()
-print cross_validation.cross_val_predict(clf, X, y, cv=5)
+#print cross_validation.cross_val_predict(clf, X, y, cv=5)
 #clf=RandomForestRegressor(n_estimators= 60, max_depth=13, min_samples_split=110,
  #                                 min_samples_leaf=20,max_features='sqrt' ,oob_score=True, random_state=10)
 #clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
 clf.fit(X, y)
 
-df=pd.read_csv("add_Week_result/5_12_10min.csv")
-df_row=df[df["监测点id"]==20104605]
-week=df[df["监测点id"]==20104605]["Week"].as_matrix()[0]
-    
+df=pd.read_csv("add_Week_result/5_21_10min.csv")
+last_day_df=pd.read_csv("add_Week_result/5_20_10min.csv")
+last_day_row=last_day_df[last_day_df["监测点id"]==10100406]
+df_row=df[df["监测点id"]==10100406]
+week=df[df["监测点id"]==10100406]["Week"].as_matrix()[0]
+last_day_temp=last_day_row.filter( regex="[0-9].*")
 df_temp=df_row.filter( regex="[0-9].*")
-df_final=pd.DataFrame({"nums":df_temp.T[df_temp.T.columns[0]],"time_id":range(0,144)})
+df_final=pd.DataFrame({"nums":df_temp.T[df_temp.T.columns[0]].as_matrix(),"last_day":last_day_temp.T[last_day_temp.T.columns[0]].as_matrix(),"time_id":range(0,144)},columns=["nums","last_day","time_id"])
 df_final.to_csv("temp.csv",index=False)
 df_final=pd.read_csv("temp.csv")
 df_final['week']=week
@@ -39,7 +53,11 @@ for index ,row in df_final.iterrows():
         df_final.loc[index,'is_high']=1
     else:
         df_final.loc[index,'is_high']=0
-        
+
+#scaler=preprocessing.StandardScaler()
+#default_scale_param = scaler.fit(df_final['default'])
+#df_final['default_scaled'] = scaler.fit_transform(df_final['default'], default_scale_param)
+
 df_final= pd.concat([df_final,dummies_Time], axis=1)
 df_final.drop(['time_id','week'], axis=1, inplace=True)
 df_final['week_1']=0
@@ -50,19 +68,23 @@ df_final['week_5']=0
 df_final['week_6']=0
 df_final['week_7']=0
 df_final['week_3']=1
-print df_final
+#print df_final
+df_final.to_csv("test_final.csv",index=False)
 test_np=df_final.as_matrix()
 X_test=test_np[:,1:]
 y_test=test_np[:,0]
+
+#y_test=do_normalise(y_test)
 y_predict=clf.predict(X_test)
+
 df_result=pd.DataFrame({"test":y_test,"pre":y_predict})
 
 df_result.to_csv("result_week_dummied.csv",index=False)
 print clf.coef_
 sub=df_result['test']-df_result['pre']
 
-
-
+score=np.abs(df_result['test']-df_result['pre']/df_result['test']).mean()
+print score
 fig = plt.figure()
 fig.set(alpha=0.2)  # 设定图表颜色alpha参数
 df_result['test'].plot()
